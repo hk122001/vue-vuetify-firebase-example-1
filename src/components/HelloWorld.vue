eb034d075535bf53394c6fc73dfc2ad3297c008b
rez1619 <hlibk@proton.me>
2023-03-31 21:39:07 -0600

    Fixed v-dialog and added several design changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,24 @@
 <script setup>
   import { ref } from 'vue'
   import materias1 from "./materias.json"
-  const obj = JSON.parse(JSON.stringify(materias1))
+  import componentClasses from "./componentClasses.vue"
 
-  let numberOfSubjects = obj.materias.length;
   let dialog = ref(false)
-  let dialogArray = []
-  for(let i = 0; i < numberOfSubjects; i++){
-    dialogArray.push(false)
-  }
 </script>
 
 <template>
-  <v-sheet v-for="index in numberOfSubjects" v-bind="index" :value="index" :key="index" class="pa-6 mx-auto my-6" max-width="900" elevation="2" rounded="xl">
-    <h3>{{ obj.materias[index-1].title }}</h3>
-    <p class="text-primary">Profesor: {{ obj.materias[index-1].professor }}</p>
-    <v-row class="dialog_row">
-      <v-spacer />
-      <v-btn
-        color="#384FFE"
-        variant="text"
-        @click="dialogArray[index-1] = true"
-        >
-        <p class="text-primary font-weight-bold capitalize text-h6">Ver detalles</p>
-      </v-btn>
-      <v-dialog v-model="dialogArray[index-1]">
-        <v-sheet class="pa-8 mx-auto" width="100%" max-width="800" elevation="2" rounded="xl">
-          <h3 class="dialog_title">{{ obj.materias[index-1].title }}</h3>
-          <div class="dialog_info">
-            <p class="text-secondary">Profesor: {{ obj.materias[index-1].professor }}</p>
-            <p class="text-secondary">Salón: {{ obj.materias[index-1].classroom }}</p>
-            <p class="text-secondary">Días: {{ obj.materias[index-1].days }}</p>
-            <p class="text-secondary">Horario: {{ obj.materias[index-1].schedule }}</p>
-          </div>
-          <v-row>
-            <v-spacer />
-            <v-btn
-              color="#384FFE"
-              block
-              variant="text"
-              @click="dialogArray[index-1] = false"
-              >
-              <p class="text-primary font-weight-bold text-h6">CERRAR</p>
-            </v-btn>
-          </v-row>
-        </v-sheet>
-      </v-dialog>
-    </v-row>
-  </v-sheet>
+  <v-container align="center">
+    <h1 class="text-primary text-h3 font-weight-bold">MATERIAS INSCRITAS</h1>
+  </v-container>
+  <componentClasses v-for="(materia, index) in materias1.materias" :key="index" v-bind="materia"></componentClasses>
 </template>
 
 <script>
   export default {
-    data () {
+    data() {
       return {
         dialog: false,
       }
-    },
+    }
   }
 </script>
---
+++ src/components/componentClasses.vue
@@ -0,0 +1,82 @@
+<script setup>
+  import { ref, defineProps } from 'vue'
+  let dialog = ref(false)
+  const props = defineProps({
+    professor: String,
+    classroom: String,
+    title: String,
+    days: Array,
+    schedule: String,
+  })
+  function calculateDays(daysArray) {
+    let returnString = ""
+    for(let i = 0; i < daysArray.length; i++){
+      if(i == daysArray.length - 2){
+        returnString = returnString + `${daysArray[i]} `
+      }
+      else if(i != daysArray.length - 1){
+        returnString = returnString + `${daysArray[i]}, `
+      }else{
+        returnString = returnString + `& ${daysArray[i]}`
+      }
+    }
+    return returnString
+  }
+  let calculatedDays = calculateDays(props.days)
+</script>
+
+<template>
+  <v-sheet class="pa-6 mx-auto my-6 rounded-lg" max-width="900" elevation="2">
+    <p class="text-primary titles">{{ title }}</p>
+    <p class="text-secondary cards">Profesor: {{ professor }}</p>
+    <v-row class="dialog_row">
+      <v-spacer />
+      <v-dialog v-model="dialog">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            color="#384FFE"
+            variant="text"
+            v-bind="props"
+            >
+            <p class="text-primary font-weight-bold capitalize text-h6">Ver detalles</p>
+          </v-btn>
+        </template>
+        <v-sheet class="pa-8 mx-auto rounded-lg" width="100%" max-width="800" elevation="2">
+          <h1 class="text-primary text-h4 font-weight-bold" align="center">{{ title }}</h1>
+          <div class="dialog_info">
+            <br>
+            <p class="text-secondary cards">Profesor: {{ professor }}</p>
+            <p class="text-secondary cards">Salón: {{ classroom }}</p>
+            <p class="text-secondary cards">Días: {{ calculatedDays }}</p>
+            <p class="text-secondary cards">Horario: {{ schedule }}</p>
+            <br>
+          </div>
+          <v-row>
+            <v-col></v-col>
+            <v-col></v-col>
+            <v-col>
+              <v-btn
+                color="#384FFE"
+                block
+                variant="text"
+                @click="dialog = false"
+                >
+                <p class="text-primary font-weight-bold text-h6">CERRAR</p>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-dialog>
+    </v-row>
+  </v-sheet>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dialog: false,
+      }
+    }
+  }
+</script>
